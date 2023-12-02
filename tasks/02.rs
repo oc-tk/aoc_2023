5d2e93872ffb7343fc21bd6098ca782c13422aa2
Tomasz Kaczmarzyk <tspamm3r@gmail.com>
2023-12-02 14:11:33 +0100

    mv task 02 to completed

--- tasks/02.rs
+++ tasks/02.rs
@@ -1,3 +1,14 @@
+use std::fs::read_to_string;
+use regex::Regex;
+use std::collections::HashMap;
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
 
 fn main() {
     let mut sum:i32 = 0;
